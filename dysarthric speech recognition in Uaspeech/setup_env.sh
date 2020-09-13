0495e05450c8003e15d0967ffec1a5378c9bb909
yhzhouowo <54799259+zhouyh-jlu@users.noreply.github.com>
2020-09-13 09:27:42 +0100

    Update setup_env.sh

--- dysarthric speech recognition in Uaspeech/setup_env.sh
+++ dysarthric speech recognition in Uaspeech/setup_env.sh
@@ -1,3 +1,4 @@
+    # #local running
     # module load apps/python/conda
     # source activate myenv
     # module load libs/CUDA/9.0.176/binary
@@ -7,6 +8,7 @@
     # PATH=$PATH:/usr/local/packages/libs/icu/58.2/gcc-4.9.4/bin
     # module load libs/intel-mkl/2019.3/binary
     # module load libs/libsndfile/1.0.28/gcc-4.9.4
+# In the HPC named ShARC
 if [[ "$SGE_CLUSTER_NAME" == "sharc" ]]; then
   module load apps/python/conda	    
   module load libs/CUDA/9.0.176/binary		    
@@ -17,7 +19,7 @@ if [[ "$SGE_CLUSTER_NAME" == "sharc" ]]; then
   module load libs/intel-mkl/2019.3/binary
 
   # Add SOX PATH
-  LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/act19yz/tools/sox/lib
+  LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/username/tools/sox/lib
   PATH=$PATH:/home/act19yz/tools/sox/bin
   source activate success
   conda install pandas
